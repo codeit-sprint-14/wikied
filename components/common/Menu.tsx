57eac01cd45b63391631982cb153d081616b3858
solmin <smin2020@icloud.com>
2025-04-24 10:37:32 +0900

    ✨Feat: GNB, Menu Component, Checking Input

---
+++ components/common/Menu.tsx
@@ -0,0 +1,54 @@
+import color from '@/utils/color';
+import typo from '@/utils/typo';
+import styled, { css } from 'styled-components';
+
+const MenuContainer = styled.ul<{ isOpen: boolean }>`
+  position: absolute;
+  top: 38px;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+
+  width: 110px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+  background: ${color('gray50')};
+
+  transition: all 0.1s ease-out;
+  transform: ${props =>
+    props.isOpen ? 'scaleY(1) translateY(0)' : 'scaleY(0.95) translateY(-6px)'};
+  opacity: ${props => (props.isOpen ? '1' : '0')};
+  pointer-events: ${props => (props.isOpen ? 'auto' : 'none')};
+  transform-origin: top;
+`;
+
+const MenuItemContainer = styled.li`
+  width: 100%;
+  height: 42px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  transition: all 0.1s ease-out;
+  border-bottom: 1px solid ${color('gray100')};
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  ${typo('14m')};
+  color: ${color('gray500')};
+
+  &:hover {
+    background: ${color('gray100')};
+  }
+`;
+
+function MenuItem({
+  children,
+  ...rest
+}: { children: React.ReactNode } & React.HTMLAttributes<HTMLLIElement>) {
+  return <MenuItemContainer {...rest}>{children}</MenuItemContainer>;
+}
+
+export { MenuContainer, MenuItem };
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -5,11 +5,24 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Sign from '@/styles/sign';
 import { signIn } from 'next-auth/react';
+import useInputConfirm from '@/hooks/useInputConfirm';
+import SignInput from '@/components/feature/SignInput';
 
 export default function Signup() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const email = useInputConfirm();
+  const pw = useInputConfirm();
+
+  function passInputs() {
+    if (email.msg || pw.msg) {
+      return false;
+    }
+
+    return true;
+  }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,17 +32,6 @@ export default function Signup() {
     const formData = new FormData(event.currentTarget);
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
-
-    // if (password !== passwordConfirmation) {
-    //   setError('비밀번호가 일치하지 않습니다.');
-    //   setIsLoading(false);
-    //   return;
-    // }
-    // if (password.length < 6) {
-    //   setError('비밀번호는 6자 이상이어야 합니다.');
-    //   setIsLoading(false);
-    //   return;
-    // }
 
     try {
       const result = await signIn('credentials', {
@@ -57,9 +59,31 @@ export default function Signup() {
     <Sign>
       <form className="signContainer" onSubmit={handleSubmit}>
         <h1>어서와요</h1>
-        <label htmlFor="email">
+        <SignInput
+          inputState={email}
+          name={'email'}
+          placeholder={'이메일을 입력해주세요'}
+          type={'text'}
+          title={'이메일'}
+          autoComplete={'email'}
+          id={'email'}
+        />
+        <SignInput
+          inputState={pw}
+          name={'password'}
+          placeholder={'비밀번호를 입력해주세요'}
+          type={'password'}
+          title={'비밀번호'}
+          autoComplete={'new-password'}
+          id={'password'}
+          pw={pw.value}
+          value={pw.value}
+          onChange={pw.onChange}
+        />
+        {/* <label htmlFor="email">
           이메일
           <Input name="email" id="email" type="email" placeholder="이메일을 입력해주세요" />
+          {error && <span className="error">{error}</span>}
         </label>
         <label htmlFor="password">
           비밀번호
@@ -69,8 +93,9 @@ export default function Signup() {
             type="password"
             placeholder="비밀번호를 입력해주세요"
           />
-        </label>
-        <Button type="submit" width="100%">
+          {error && <span className="error">{error}</span>}
+        </label> */}
+        <Button type="submit" width="100%" disabled={!passInputs()}>
           로그인
         </Button>
 
