38cd46925c3704f27f52948b9808a036f0ae3bfb
solmin <smin2020@icloud.com>
2025-05-01 11:48:47 +0900

    ✨ Feat: add /wikilist page

--- pages/wikilist/index.tsx
+++ pages/wikilist/index.tsx
@@ -1,5 +1,238 @@
+import color from '@/utils/color';
+import axios from 'axios';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import Thumbnail from '@/public/images/img-thumbnail.png';
+import typo from '@/utils/typo';
+import Input from '@/components/common/Input';
+import Search from '@/public/icons/ico-search.svg';
+import NotFound from '@/public/images/img-not-found.svg';
+
+const Container = styled.div`
+  width: 860px;
+  margin: 164px auto 480px;
+
+  .search-container {
+    position: relative;
+    width: 100%;
+    margin-bottom: 16px;
+    .search-icon {
+      position: absolute;
+      left: 20px;
+      top: 50%;
+      transform: translateY(-50%);
+      z-index: 10;
+    }
+    input {
+      padding-left: 58px;
+    }
+  }
+
+  .total-count {
+    display: block;
+    ${typo('16r')}
+    color: ${color('gray400')};
+    margin-bottom: 34px;
+    margin-left: 12px;
+
+    b {
+      ${typo('16r')}
+      color: ${color('green300')};
+    }
+  }
+`;
+
+const ListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  .no-result {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+    margin-top: 48px;
+    ${typo('16r')};
+    color: ${color('gray400')};
+
+    b {
+      ${typo('16sb')};
+    }
+  }
+
+  li {
+    display: flex;
+    gap: 32px;
+    border-bottom: 1px solid ${color('gray200')};
+    cursor: pointer;
+    width: 100%;
+    height: 142px;
+    align-items: center;
+    padding: 0 24px;
+    transition: all 0.1s ease-out;
+
+    &:hover {
+      background: hsla(232, 18.7%, 47.3%, 0.03);
+    }
+
+    .contents {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      color: ${color('gray400')};
+    }
+
+    .contents-top {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      color: ${color('gray400')};
+
+      h2 {
+        ${typo('24sb')}
+        color: ${color('gray500')};
+      }
+    }
+
+    .image-container {
+      width: 94px;
+      height: 94px;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+`;
+
 function WikiList() {
-  return <div>WikiList</div>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [orderBy, setOrderBy] = useState('recent');
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(100);
+  const [totalCount, setTotalCount] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://wikied-api.vercel.app/14-6/profiles?page=${page}&pageSize=${pageSize}&name=${search}`
+        );
+        const userList = await Promise.all(
+          res.data.list.map(async (v: any) => {
+            try {
+              const detailRes = await axios.get(
+                `https://wikied-api.vercel.app/14-6/profiles/${v.code}`
+              );
+              const detailList = detailRes.data;
+              return detailList;
+            } catch (err) {
+              console.error(err);
+            }
+          })
+        );
+        setArticles(userList);
+        setTotalCount(res.data.totalCount);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [search, orderBy, page, pageSize]);
+
+  return (
+    <Container>
+      <div className="search-container">
+        <Image className="search-icon" src={Search} alt="search" />
+        <Input
+          placeholder="닉네임 검색"
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              setSearch(e.target.value);
+            }
+          }}
+        />
+      </div>
+      {totalCount > 0 && (
+        <span className="total-count">
+          {search ? (
+            <>
+              <b>{totalCount}</b>명의 프로필을 찾았어요
+            </>
+          ) : (
+            <>
+              위키드에 <b>{totalCount}</b>명의 프로필이 있어요
+            </>
+          )}
+        </span>
+      )}
+      <ListContainer>
+        {articles.length > 0 ? (
+          articles.map(article => (
+            <li key={article.id} onClick={() => router.push(`/wiki/${article.code}`)}>
+              <div className="image-container">
+                {article.image ? (
+                  <Image src={encodeURI(article.image)} alt={article.name} width={94} height={94} />
+                ) : (
+                  <Image src={Thumbnail} alt={article.name} width={94} height={94} />
+                )}
+              </div>
+              <div className="contents">
+                <div className="contents-top">
+                  <h2>{article.name}</h2>
+                  <span>{article.birthday || ''}</span>
+                  <span>{article.job || ''}</span>
+                </div>
+                <p>{article.content ? article.content.replace(/<[^>]*>?/gm, '') : '소개글 없음'}</p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <div className="no-result">
+            <Image src={NotFound} alt="not found" />
+            <span>
+              <b>{search}</b>님을 찾지 못했어요 :(
+            </span>
+          </div>
+        )}
+      </ListContainer>
+    </Container>
+  );
 }
 
 export default WikiList;
+
+// {
+//     "id": 725,
+//     "code": "5575c062-5cea-40a4-98c3-822dbba48169",
+//     "image": "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wikied/user/2394/1745690443010/%C3%AC%C2%8A%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A6%C2%B0%C3%AC%C2%83%C2%B7%202025-03-31%20160610.png",
+//     "city": "",
+//     "mbti": "",
+//     "job": "",
+//     "sns": "",
+//     "birthday": "",
+//     "nickname": "",
+//     "bloodType": "",
+//     "family": "",
+//     "nationality": "",
+//     "content": "<p>안녕하세요 물개입니다.</p>",
+//     "teamId": "14-6",
+//     "securityQuestion": "질문",
+//     "updatedAt": "2025-04-26T18:00:51.356Z",
+//     "name": "김성빈225"
+//   },
